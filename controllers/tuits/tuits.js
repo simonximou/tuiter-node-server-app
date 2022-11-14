c43cb37c8049412446c9e139b770fd59a85356e0
Xi Zhang <71995672+simonximou@users.noreply.github.com>
2022-11-13 18:48:57 -0800

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "_id": "123", "topic": "Space", "name": "SpaceX",
+        "userName": "@spacex", "time": "2h", "profileImg": "https://www.spacex.com/static/images/share.jpg",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "likes": 2345,
+        "comment": 123, "retuit": 432, "dislikes":2
+    },
+    {
+        "_id": "234", "topic": "Traffic", "name": "The Boring Company",
+        "userName": "@boringcompany", "time": "2h", "profileImg": "https://i.pinimg.com/736x/ff/d3/6c/ffd36ce1af1b8b3c6edce349a07046f3.jpg",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": true, "likes": 2345,
+        "comment": 123, "retuit": 432, "dislikes":2
+    },
+];
