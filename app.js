8cb1205b18d5abce27163ee284b24ec8f2dcd020
Xi Zhang <71995672+simonximou@users.noreply.github.com>
2022-11-29 23:30:19 -0800

    a9 fall22 5610

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import TuitsController
 const app = express()
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
+    || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 app.listen(process.env.PORT || 4000);
 app.use(cors())
